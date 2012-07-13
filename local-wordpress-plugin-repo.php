0abbea8fc54f1e80afebce6293f6dae43bcf5cdc
jeremyfelt <jeremy.felt@gmail.com>
2012-07-13 16:53:00 -0700

    Use a variable for the post type so that it can be easily changed by anyone using this plugin.

--- local-wordpress-plugin-repo.php
+++ local-wordpress-plugin-repo.php
@@ -2,6 +2,14 @@
 
 class Local_WordPress_Plugin_Repo_Foghlaim {
 
+	/**
+	 * Post type slug to use for storing plugin information from the repo
+	 *
+	 * @access public
+	 * @var string
+	 */
+	var $post_type = 'fog_lpr_plugin';
+
 	public function __construct() {
 		add_action( 'init', array( $this, 'create_content_type' ) );
 		add_action( 'after_setup_theme', array( $this, 'event_scheduler' ) );
@@ -12,7 +20,7 @@ class Local_WordPress_Plugin_Repo_Foghlaim {
 	}
 
 	public function create_content_type() {
-		register_post_type( 'fog_lpr_plugin',
+		register_post_type( $this->post_type,
 			array(
 			     'labels' => array(
 				     'name' => 'WordPress Plugins',
@@ -53,7 +61,7 @@ class Local_WordPress_Plugin_Repo_Foghlaim {
 	function check_plugin_api() {
 		$plugin_posts = new WP_Query( array(
 		                                   'posts_per_page' => 25,
-		                                   'post_type' => 'fog_lpr_plugin',
+		                                   'post_type' => $this->post_type,
 		                                   'no_found_rows' => true,
 		                              ));
 
@@ -146,7 +154,7 @@ class Local_WordPress_Plugin_Repo_Foghlaim {
 
 	function get_plugin_stats( $post_id, $post = 0, $manual = false ) {
 
-		if ( ! empty( $post ) && 'fog_lpr_plugin' != $post->post_type && ! $manual )
+		if ( ! empty( $post ) && $this->post_type != $post->post_type && ! $manual )
 			return;
 
 		$current_slug = get_post_meta( $post_id, '_fog_lpr_plugin_slug', true );
@@ -179,7 +187,7 @@ class Local_WordPress_Plugin_Repo_Foghlaim {
 
 	function get_plugin_support_feed( $post_id, $post = 0, $manual = false ) {
 
-		if ( ! empty( $post ) && 'fog_lpr_plugin' != $post->post_type && ! $manual )
+		if ( ! empty( $post ) && $this->post_type != $post->post_type && ! $manual )
 			return;
 
 		$current_slug = get_post_meta( $post_id, '_fog_lpr_plugin_slug', true );
