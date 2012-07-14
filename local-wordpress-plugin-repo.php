cb35259bef034444849568a8ef23300ed30d57d8
jeremyfelt <jeremy.felt@gmail.com>
2012-07-13 17:01:55 -0700

    should add the plugin stuff and license, yeah?

--- local-wordpress-plugin-repo.php
+++ local-wordpress-plugin-repo.php
@@ -1,4 +1,29 @@
 <?php
+/*
+Plugin Name: Local WordPress Plugin Repo
+Plugin URI: http://jeremyfelt.com/wordpress/plugins/local-wordpress-plugin-repo/
+Description: Regularly checks WordPress repository information for your plugins and stores in WordPress
+Version: 0.9
+Author: Jeremy Felt
+Author URI: http://jeremyfelt.com
+License: GPL2
+*/
+
+/*  Copyright 2012 Jeremy Felt (email: jeremy.felt@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 class Local_WordPress_Plugin_Repo_Foghlaim {
 
